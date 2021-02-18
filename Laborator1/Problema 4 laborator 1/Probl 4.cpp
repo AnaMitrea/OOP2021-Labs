e376267624b74071affb52389dfc002ceb1a6fc7
Annee <anamitrea27@gmail.com>
2021-02-18 11:43:56 +0200

    Problema 4

---
+++ Laborator1/Problema 4 laborator 1/Probl 4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+
+{
+
+    for (int tr = 2; tr < n / ......; tr++)
+
+        if ((n % ......) == 0)
+
+            return ......;
+
+    return true;
+
+}
+
+int main()
+
+{
+
+    int n;
+
+    std::cout << "Enter a number:";
+
+    std::cin >> ......;
+
+    if (isPrime(n))
+
+        std::cout << n << " is prime !";
+
+    else
+
+        std::cout << n << " is NOT prime !";
+
+    return 0;
+
+}
