e7a114ae8c2dc9ce65dba1aa47102c1f3d34d978
Annee <anamitrea27@gmail.com>
2021-02-25 11:40:50 +0200

    Laborator 2- Problema 1

--- Laborator 2/Problema 1 Laborator 2/main.cpp
+++ Laborator 2/Problema 1 Laborator 2/main.cpp
@@ -7,12 +7,15 @@ int main()
 {
 	NumberList obiect;
 	obiect.Init();
+
 	int x = 5;
 	obiect.Add(x);
 	obiect.Add(6);
 	obiect.Add(2);
+
 	printf("Vectorul meu este: ");
 	obiect.Print();
+
 	obiect.Sort();
 	printf("Vectorul meu dupa sortare este: ");
 	obiect.Print();
